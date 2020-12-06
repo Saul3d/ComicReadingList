737eda05ba24417ace923ff51a7307dfbc63a5e5
Saul Solano <saul3d@yahoo.com>
2020-12-06 14:57:57 -0600

    fixed http/https issue

--- src/helpers/data/getCharacterData.js
+++ src/helpers/data/getCharacterData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import apiKeys from '../apiKeys.json';
 
 const getCharacters = () => new Promise((resolve, reject) => {
-  axios.get(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${apiKeys.marvelApiKeys}&limit=100`)
+  axios.get(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${apiKeys.marvelApiKeys}&limit=100`)
     .then((res) => {
       const characters = [];
       const allData = res.data.data.results;
